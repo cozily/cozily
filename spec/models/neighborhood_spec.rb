755fbf44d00d8a0ebe9a780ecc6b27a6f4d89d8b
Todd And Haddad <pivotal@walker.(none)>
2010-06-16 16:50:35 -0400

    adds a spec for Neighborhood.for_lat_and_lng

--- spec/models/neighborhood_spec.rb
+++ spec/models/neighborhood_spec.rb
@@ -5,4 +5,19 @@ describe Neighborhood do
   it { should have_many(:apartments, :through => :addresses) }
 
   it { should validate_presence_of(:name) }
+
+  describe ".for_lat_and_lng" do
+    it "creates a neighorhood based on the response from Yelp" do
+      lambda {
+        Neighborhood.for_lat_and_lng(40.6824793, -74.0003197)
+      }.should change(Neighborhood, :count).by(1)
+
+      neighborhood = Neighborhood.last
+      neighborhood.name.should == "Carroll Gardens"
+      neighborhood.city.should == "New York"
+      neighborhood.state.should == "NY"
+      neighborhood.country.should == "USA"
+      neighborhood.borough.should == "Brooklyn"
+    end
+  end
 end
