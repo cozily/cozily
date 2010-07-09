346823acade7489cd5332da0e335489db7cb700b
Todd Persen <todd.persen@gmail.com>
2010-07-09 15:30:22 -0400

    Add neighborhoods to profiles.

--- features/profiles.feature
+++ features/profiles.feature
@@ -1,4 +1,5 @@
 Feature: Profiles
   Scenario: User creates a profile
     Given I am a logged in user
+    And an apartment exists with state: "listed"
     Then I can create a profile
--- features/step_definitions/profile_steps.rb
+++ features/step_definitions/profile_steps.rb
@@ -3,9 +3,11 @@ Then /^I can create a profile$/ do
 
   fill_in "Bedrooms", :with => "2"
   fill_in "Rent", :with => "2000.00"
+  select Apartment.last.neighborhoods.first.name, :from => "Neighborhoods"
 
   click_button "Save Profile"
   the.user.profile.should_not be_nil
   the.user.profile.bedrooms.should == 2.0
   the.user.profile.rent.should == 2000.00
+  the.user.profile.neighborhoods.should == [Apartment.last.neighborhoods.first]
 end
