65ae5a357a32382df647ff24e207408ac94bbb7b
Pivotal Labs <pivotal@spring.(none)>
2010-07-15 11:29:25 -0400

    renders message if mobile device

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,8 +8,15 @@ class ApplicationController < ActionController::Base
   geocode_ip_address
   has_mobile_fu
 
+  before_filter :check_mobile
+
   rescue_from CanCan::AccessDenied do |exception|
     flash[:error] = exception.message
     redirect_to root_url
+  end
+
+  private
+  def check_mobile
+    render :text => "mobile version coming soon" if is_mobile_device?
   end
 end
