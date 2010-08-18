8e0601a41e945cf37226cf1c56914da5ac389c8f
Michael Haddad <michael@ludditetechnology.com>
2010-08-17 22:10:54 -0400

    removes mobile_fu

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 require 'geokit-rails'
-require 'mobile_fu'
 
 class ApplicationController < ActionController::Base
   include Clearance::Authentication
@@ -7,21 +6,9 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   geocode_ip_address
-  has_mobile_fu
-
-  layout :set_layout
 
   rescue_from CanCan::AccessDenied do |exception|
     flash[:error] = exception.message
     redirect_to root_url
-  end
-
-  private
-  def set_layout
-    if is_mobile_device?
-      "mobile"
-    else
-      "application"
-    end
   end
 end
