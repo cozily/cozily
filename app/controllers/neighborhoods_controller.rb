dcc6742785a65e8e7e5201d90f6349de01d06796
Michael Haddad <michael@ludditetechnology.com>
2010-09-04 01:28:04 -0400

    adds styling for neighborhood show

--- app/controllers/neighborhoods_controller.rb
+++ app/controllers/neighborhoods_controller.rb
@@ -5,7 +5,16 @@ class NeighborhoodsController < ApplicationController
 
   def show
     @neighborhood = Neighborhood.find(params[:id])
-    @apartments = @neighborhood.listed_apartments
+    @apartments = @neighborhood.listed_apartments.paginate(:page => params[:page], :per_page => Apartment.per_page)
+
+    respond_to do |format|
+      format.html {}
+      format.js do
+        render :json => { :apartments => render_to_string(:partial => "neighborhoods/apartments",
+                                                          :locals => { :apartments => @apartments } ),
+                          :map_others => @apartments.as_json(:methods => :to_param, :include => :address).to_json }
+      end
+    end
   end
 
   def search
