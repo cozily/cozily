40089da9ca28dcb20260642b10a93f1a4a134a85
Michael Haddad <michael@ludditetechnology.com>
2010-09-06 01:58:05 -0400

    dashboard matches tab shows profile summary

---
+++ app/helpers/users_helper.rb
@@ -0,0 +1,26 @@
+include NeighborhoodsHelper
+
+module UsersHelper
+  def profile_summary(profile)
+    summary = ["* matching"]
+    summary << if profile.try(:bedrooms)
+      "#{profile.bedrooms.prettify} bedroom apartments"
+    else
+      "apartments with any number of bedrooms"
+    end
+
+    summary << if profile.try(:rent)
+      "under $#{number_with_delimiter(profile.rent)}"
+    else
+      "regardless of rent"
+    end
+
+    summary << if profile.try(:neighborhoods).try(:present?)
+      "in #{neighborhood_links(profile.neighborhoods)}"
+    else
+      "in all neighborhoods"
+    end
+
+    summary.join(" ")
+  end
+end
---
+++ spec/helpers/users_helper_spec.rb
@@ -0,0 +1,42 @@
+require 'spec_helper'
+
+describe UsersHelper do
+  describe "profile_summary" do
+    before do
+      @neighborhood = Factory(:neighborhood,
+                              :name => "Red Hook")
+      @profile = Factory(:profile,
+                         :bedrooms => 1,
+                         :rent => 1500,
+                         :neighborhoods => [@neighborhood])
+    end
+
+    it "returns a summary including the bedrooms, rent, and neighborhoods" do
+      helper.profile_summary(@profile).should == "* matching 1 bedroom apartments under $1,500 in #{link_to(@neighborhood.name, @neighborhood)}"
+    end
+
+    it "returns an appropriate summary when neighborhoods is empty" do
+      @profile.update_attribute(:neighborhoods, [])
+      helper.profile_summary(@profile).should == "* matching 1 bedroom apartments under $1,500 in all neighborhoods"
+    end
+
+    it "returns an appropriate summary when rent is nil and neighborhoods is empty" do
+      @profile.update_attributes(:rent => nil, :neighborhoods => [])
+      helper.profile_summary(@profile).should == "* matching 1 bedroom apartments regardless of rent in all neighborhoods"
+    end
+
+    it "returns an appropriate summary when bedrooms is nil and rent is nil and neighborhoods is empty" do
+      @profile.update_attributes(:bedrooms => nil, :rent => nil, :neighborhoods => [])
+      helper.profile_summary(@profile).should == "* matching apartments with any number of bedrooms regardless of rent in all neighborhoods"
+    end
+
+    it "returns an appropriate summary when bedrooms is nil and neighborhoods is empty" do
+      @profile.update_attributes(:bedrooms => nil, :neighborhoods => [])
+      helper.profile_summary(@profile).should == "* matching apartments with any number of bedrooms under $1,500 in all neighborhoods"
+    end
+
+    it "returns an appropraite summary when profile is nil" do
+      helper.profile_summary(nil).should == "* matching apartments with any number of bedrooms regardless of rent in all neighborhoods"
+    end
+  end
+end
