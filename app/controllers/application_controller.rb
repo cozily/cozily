bda55bb508b3ed042b0a90f2df85eb82046431a6
Todd Persen <todd.persen@gmail.com>
2010-09-07 00:12:54 -0400

    Clean up 404 and 500 errors.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,4 +11,35 @@ class ApplicationController < ActionController::Base
     flash[:error] = exception.message
     redirect_to root_url
   end
+
+  def render_optional_error_file(status_code)
+    if status_code == :not_found
+      render_404
+    elsif status_code == :internal_server_error
+      render_500
+    else
+      super
+    end
+  end
+
+  def render_404
+    respond_to do |type|
+      type.html {
+        render :template => "errors/404", :layout => 'application', :status => 404
+      }
+      type.all { render :nothing => true, :status => 404 }
+    end
+    true
+  end
+
+  def render_500
+    respond_to do |type|
+      type.html {
+        render :template => "errors/500", :layout => 'application', :status => 500
+      }
+      type.all { render :nothing => true, :status => 500 }
+    end
+    true
+  end
+
 end
