a79faedc43eebfc6e81deb8c84ae2b80e4e9c6c6
Michael Haddad <michael@ludditetechnology.com>
2010-09-07 22:16:22 -0400

    style contact tab

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -203,7 +203,7 @@ function liveNeighborhoodAutocomplete() {
             tabLocation: 'left',                      //side of screen where tab lives, top, right, bottom, or left
             speed: 300,                               //speed of animation
             action: 'click',                          //options: 'click' or 'hover', action to trigger animation
-            topPos: '200px',                          //position from the top/ use if tabLocation is left or right
+            topPos: '175px',                          //position from the top/ use if tabLocation is left or right
             leftPos: '20px',                          //position from left/ use if tabLocation is bottom or top
             fixedPosition: true                      //options: true makes it stick(fixed position) on scroll
         });
