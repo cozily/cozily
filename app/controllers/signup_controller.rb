029149db99353ce44114eec579d6727055aad8d6
Michael Haddad <michael@ludditetechnology.com>
2010-09-09 00:59:55 -0400

    style errors on lister cartoon signup

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -1,14 +1,31 @@
 class SignupController < ApplicationController
   def profile
     session[:want] = params[:want]
+    @user = User.new
     render :json => { :want => render_to_string(:partial => "signup/profile") }
   end
 
   def account
-    session[:profile] = params[:profile] if params.has_key?(:profile)
-    session[:user] = params[:user] if params.has_key?(:user)
-    @user = User.new
-    render :json => { :account => render_to_string(:partial => "signup/account") }
+    if params.has_key?(:profile)
+      session[:profile] = params[:profile]
+      @user = User.new
+      render :json => { :account => render_to_string(:partial => "signup/account") }
+    else
+      session[:user] = params[:user]
+      @user = User.new(params[:user].merge(:roles => [Role.find_by_name("lister")]))
+      @user.valid?
+      if @user.errors.any? { |attr, msg| attr == "email" || attr == "phone" }
+        @errors = @user.errors.dup
+        @user.errors.clear
+        @errors.each do |attr, msg|
+          @user.errors.add(attr, msg) if attr == "email" || attr == "phone"
+        end
+        render :json => { :want => render_to_string(:partial => "signup/profile") }
+      else
+        @user = User.new
+        render :json => { :account => render_to_string(:partial => "signup/account") }
+      end
+    end
   end
 
   def create
