baf06d69abee7f16cc1ecee4ebe53e76cb1624d9
Michael Haddad <michael@ludditetechnology.com>
2010-09-12 17:14:33 -0400

    message body cannot be default body

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,9 +1,12 @@
 class Message < ActiveRecord::Base
+  DEFAULT_BODY = "Contact Landlord/Agent..."
+
   belongs_to :apartment
   belongs_to :sender, :class_name => "User"
   belongs_to :conversation
 
   validates_presence_of :sender, :body, :conversation
+  validate :ensure_body_is_not_default_body
 
   named_scope :unread, :conditions => { :read_at => nil }
   named_scope :sent_by, lambda { |user| { :conditions => ["sender_id = ?", user.id] } }
@@ -16,6 +19,10 @@ class Message < ActiveRecord::Base
   end
 
   private
+  def ensure_body_is_not_default_body
+    errors.add(:body, "must be different than the default") if body == Message::DEFAULT_BODY
+  end
+
   def notify_receiver
     MessageMailer.deliver_receiver_notification(self)
   end
--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -9,6 +9,12 @@ describe Message do
     it { should validate_presence_of(attr) }
   end
 
+  it "should be invalid with the default body" do
+    message = Factory.build(:message, :body => Message::DEFAULT_BODY)
+    message.should be_invalid
+    message.should have(1).error_on(:body)
+  end
+
   it "should e-mail the receiver after create" do
     conversation = Factory.build(:conversation)
     MessageMailer.should_receive(:deliver_receiver_notification)
