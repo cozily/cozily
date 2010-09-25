c85bafb191859a702c7d017ca7edd549b3e01b1b
Todd Persen <todd.persen@gmail.com>
2010-09-25 02:59:35 -0400

    Change feedback email to support@cozi.ly

--- app/mailers/feedback_mailer.rb
+++ app/mailers/feedback_mailer.rb
@@ -1,7 +1,7 @@
 class FeedbackMailer < ActionMailer::Base
   def feedback(email, message)
     from email
-    recipients "feedback@cozi.ly"
+    recipients "support@cozi.ly"
     subject "Cozily Feedback"
     body :message => message
   end
