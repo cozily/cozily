a44f6fd37f09540fc18b4c65a37d7eb299bbf0d4
Michael Haddad <michael@ludditetechnology.com>
2010-09-28 23:41:48 -0400

    messages are from cozily-noreply

--- app/mailers/match_mailer.rb
+++ app/mailers/match_mailer.rb
@@ -2,7 +2,7 @@ include UsersHelper
 
 class MatchMailer < ActionMailer::Base
   def new_match_notification(apartment, user)
-    from "support@cozi.ly"
+    from "cozily-noreply@cozi.ly"
     recipients user.email
     subject "A new apartment in #{apartment.neighborhoods.map(&:name).to_sentence} was listed on Cozily..."
     body :apartment => apartment, :user => user
--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -1,6 +1,6 @@
 class MessageMailer < ActionMailer::Base
   def receiver_notification(message)
-    from "support@cozi.ly"
+    from "cozily-noreply@cozi.ly"
     recipients message.receiver.email
     subject "#{message.sender.full_name} sent you a message on Cozily..."
     body :message => message
