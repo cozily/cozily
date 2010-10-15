cf4bbc167394f8ac8a840e637778751476a39aaa
Michael Haddad <michael@ludditetechnology.com>
2010-10-15 18:34:47 -0400

    add a lister factory

--- test/factories/clearance.rb
+++ test/factories/clearance.rb
@@ -14,3 +14,8 @@ end
 Factory.define :email_confirmed_user, :parent => :user do |user|
   user.email_confirmed { true }
 end
+
+Factory.define :lister, :parent => :email_confirmed_user do |user|
+  user.roles { [Role.find_by_name("lister")] }
+  user.phone { "202-270-7370" }
+end
