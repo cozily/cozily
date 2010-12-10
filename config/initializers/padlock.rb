2e1abaf7d1e8c9bff34bad273c0268438e545743
Michael Haddad <michael@ludditetechnology.com>
2010-12-09 20:44:45 -0500

    Padlock and disable activity feed.

--- config/initializers/padlock.rb
+++ config/initializers/padlock.rb
@@ -1,5 +1,6 @@
 include Padlock
 
 Padlock(Rails.env) do
+  disable :activity_feed, :in => [:development, :test, :staging, :production]
   disable :tweet_apartments, :in => [:development, :test, :staging, :production]
 end
