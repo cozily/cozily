8b4cb83425f596e2017a3f15ab2bca28e1de6f4c
Michael Haddad <michael@ludditetechnology.com>
2010-12-13 18:40:37 -0500

    Removes deprecation warning in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,7 +21,7 @@ class ApplicationController < ActionController::Base
     redirect_to :back
   end
 
-  unless ActionController::Base.consider_all_requests_local
+  unless Rails.application.config.consider_all_requests_local
     rescue_from Exception,                            :with => :render_error
     rescue_from ActiveRecord::RecordNotFound,         :with => :render_not_found
     rescue_from ActionController::RoutingError,       :with => :render_not_found
