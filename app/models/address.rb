e8a49eac35f43b81040a7f003eff099590359dac
Todd Persen <todd.persen@gmail.com>
2011-10-27 13:17:03 -0400

    Work around broken geokit city support.

--- app/models/address.rb
+++ app/models/address.rb
@@ -29,7 +29,7 @@ class Address < ActiveRecord::Base
         address              = Address.find_or_initialize_by_lat_and_lng(location.lat, location.lng)
         address.full_address = location.full_address
         address.street       = location.street_address
-        address.city         = location.city
+        address.city         = "New York"
         address.state        = location.state
         address.zip          = location.zip
         address.accuracy     = location.accuracy
@@ -50,7 +50,7 @@ class Address < ActiveRecord::Base
     if location.full_address.present?
       self.full_address = location.full_address
       self.street       = location.street_address
-      self.city         = location.city
+      self.city         = "New York"
       self.state        = location.state
       self.zip          = location.zip
       self.lat          = location.lat
