0a553bca8dd18679934ecb9864d1a61d0c6b74f6
Todd Persen <todd.persen@gmail.com>
2011-10-27 00:54:09 -0400

    notify_hoptoad => notify_airbrake

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -29,7 +29,7 @@ class ApplicationController < ActionController::Base
   end
 
   def render_not_found(e)
-    notify_hoptoad(e)
+    notify_airbrake(e)
     respond_to do |type|
       type.html {
         render :template => "errors/404", :layout => 'error', :status => 404
@@ -40,7 +40,7 @@ class ApplicationController < ActionController::Base
   end
 
   def render_error(e)
-    notify_hoptoad(e)
+    notify_airbrake(e)
     respond_to do |type|
       type.html {
         render :template => "errors/500", :layout => 'error', :status => 500
