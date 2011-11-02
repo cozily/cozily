660f10693a33908088eb9ae657d460724e493e6f
Todd Persen <todd.persen@gmail.com>
2011-11-02 01:01:43 -0400

    Make sure feature uniqueness is scoped correctly.

--- app/models/feature.rb
+++ app/models/feature.rb
@@ -1,5 +1,5 @@
 class Feature < ActiveRecord::Base
-  validates_uniqueness_of :name
+  validates_uniqueness_of :name, :scope => :category
 
   has_many :apartment_features, :dependent => :destroy
 end
