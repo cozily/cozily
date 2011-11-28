39399e5d8616439590b4ec47b420803a3756788f
Todd Persen <todd.persen@gmail.com>
2011-11-28 01:22:06 -0500

    Don't attempt to notify Airbrake any longer.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -29,7 +29,6 @@ class ApplicationController < ActionController::Base
   end
 
   def render_not_found(e)
-    notify_airbrake(e)
     respond_to do |type|
       type.html {
         render :template => "errors/404", :layout => 'error', :status => 404
@@ -40,7 +39,6 @@ class ApplicationController < ActionController::Base
   end
 
   def render_error(e)
-    notify_airbrake(e)
     respond_to do |type|
       type.html {
         render :template => "errors/500", :layout => 'error', :status => 500
