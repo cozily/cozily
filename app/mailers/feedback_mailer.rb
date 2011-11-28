4495000b04260ecd4ad2b48bf3a5dacd6f248608
Todd Persen <todd.persen@gmail.com>
2011-11-28 01:31:09 -0500

    Get rid of old feedback code.

--- app/mailers/feedback_mailer.rb
+++
@@ -1,8 +0,0 @@
-class FeedbackMailer < ActionMailer::Base
-  def feedback(email, message)
-    @message = message
-    from email
-    recipients "support@cozi.ly"
-    subject "Cozily Feedback"
-  end
-end
