0add3d98dd9c740dfaa4c350e34e1c2cd7df68de
Todd Persen <todd.persen@gmail.com>
2011-12-07 20:04:14 -0500

    Add new lady messages.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -49,7 +49,27 @@ module ApplicationHelper
   end
 
   def lady_messages
-    messages = ["Wow #{current_user.first_name}, that's a fabulous shirt you're wearing."]
+    messages = [
+      "Home interprets heaven. Home is heaven for beginners.",
+      "A house is not a home unless it contains food and fire for the mind as well as the body.",
+      "Be grateful for the home you have, knowing that at this moment, all you have is all you need.",
+      "Decorate your home. It gives the illusion that your life is more interesting than it really is.",
+      "Home is the most popular, and will be the most enduring of all earthly establishments.",
+      "Home is the place where, when you have to go there, they have to take you in.",
+      "Home wasn't built in a day.",
+      "Home, nowadays, is a place where part of the family waits till the rest of the family brings the car back.",
+      "If I were asked to name the chief benefit of the house, I should say: the house shelters day-dreaming, the house protects the dreamer, the house allows one to dream in peace.",
+      "Never make your home in a place. Make a home for yourself inside your own head. You'll find what you need to furnish it - memory, friends you can trust, love of learning, and other such things. That way it will go with you wherever you journey.",
+      "The fellow that owns his own home is always just coming out of a hardware store.",
+      "The home is the chief school of human virtues.",
+      "The home to everyone is to him his castle and fortress, as well for his defense against injury and violence, as for his repose.",
+      "There is nothing like staying at home for real comfort.",
+      "There are only two things we should fight for. One is the defense of our homes and the other is the Bill of Rights.",
+      "When I go home, its an easy way to be grounded. You learn to realize what truly matters.",
+      "Where we love is home - home that our feet may leave, but not our hearts."
+    ]
+
+    messages << "Wow #{current_user.first_name}, that's a fabulous shirt you're wearing."
 
     Apartment.distinct_bedrooms.each do |bedrooms|
       apartments = Apartment.where(:bedrooms => bedrooms, :sublet => false, :state => "published").order("RANDOM()")
