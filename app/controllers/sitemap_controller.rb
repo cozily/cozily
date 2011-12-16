91fad03217537152c389ecc437375e450fc80512
Todd Persen <todd.persen@gmail.com>
2011-12-16 17:54:57 -0500

    Fix XML Sitemap.

--- app/controllers/sitemap_controller.rb
+++ app/controllers/sitemap_controller.rb
@@ -2,30 +2,19 @@ class SitemapController < ApplicationController
   def index
     headers['Content-Type'] = 'application/xml'
     sitemap = XmlSitemap::Map.new('cozi.ly') do |m|
-      m.add(:url => root_path, :period => :daily, :priority => 0.1)
-      m.add(:url => browse_path, :period => :hourly, :priority => 1.0)
-      m.add(:url => about_page_path, :period => :weekly, :priority => 0.5)
-      m.add(:url => faq_page_path, :period => :weekly, :priority => 0.5)
-      m.add(:url => terms_of_service_page_path, :period => :weekly, :priority => 0.5)
-      m.add(:url => privacy_policy_page_path, :period => :weekly, :priority => 0.5)
-      m.add(:url => neighborhoods_path, :period => :weekly, :priority => 0.5)
+      m.add browse_path, period: :hourly, priority: 1.0
+      m.add about_page_path, period: :weekly, priority: 0.5
+      m.add faq_page_path, period: :weekly, priority: 0.5
+      m.add terms_of_service_page_path, period: :weekly, priority: 0.5
+      m.add privacy_policy_page_path, period: :weekly, priority: 0.5
+      m.add neighborhoods_path, period: :weekly, priority: 0.5
       Apartment.with_state(:published).each do |apartment|
-        m.add(
-          :url => apartment_path(apartment),
-          :updated => apartment.updated_at,
-          :priority => 1.0,
-          :period => :daily
-        )
+        m.add apartment_path(apartment), updated: apartment.updated_at, priority: 1.0, period: :daily
       end
       Neighborhood.all.each do |neighborhood|
-        m.add(
-          :url => neighborhood_path(neighborhood),
-          :updated => neighborhood.updated_at,
-          :priority => 1.0,
-          :period => :hourly
-        )
+        m.add neighborhood_path(neighborhood), updated: neighborhood.updated_at, priority: 1.0, period: :hourly
       end
     end
-    render :text => sitemap.render
+    render xml: sitemap.render
   end
 end
