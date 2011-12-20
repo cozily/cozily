24951d89fbb21d73c100a8c99f2eb1576898ec67
Todd Persen <todd.persen@gmail.com>
2011-12-20 14:10:46 -0500

    Use Mailgun SMTP settings for staging.

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -50,4 +50,15 @@ Cozily::Application.configure do
   config.action_mailer.default_url_options = { :host => 'staging.cozi.ly' }
 
   Sass::Plugin.options[:never_update] = true
+
+  ActionMailer::Base.default_content_type = "text/html"
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+          :enable_starttls_auto => true,
+          :address => "smtp.mailgun.org",
+          :port => 587,
+          :domain => "cozi.ly",
+          :authentication => :plain,
+          :user_name => "cozily@cozily.mailgun.org",
+          :password => "marathon69"
 end
