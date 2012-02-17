d09190066bb84c56ec2f2002f20daf74d74fa6af
Todd Persen <todd.persen@gmail.com>
2012-02-17 04:31:31 -0500

    Make sure nearby apartments is only fetching published listings.

--- app/controllers/buildings_controller.rb
+++ app/controllers/buildings_controller.rb
@@ -2,6 +2,6 @@ class BuildingsController < ApplicationController
   def show
     @building = Building.find(params[:id])
     @nearby_stations = @building.nearby_stations
-    @apartments = Apartment.find(:all, :origin => [@building.lat, @building.lng], :order => 'distance', :limit => 5)
+    @apartments = Apartment.find(:all, :origin => [@building.lat, @building.lng], :order => 'distance', :limit => 5, :conditions => "apartments.state='published'")
   end
 end
